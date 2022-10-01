f6d2e78a9cf1c7e6644d1706613c2f7a1a07d3d4
meigs2 <connorradeloff1@gmail.com>
2022-10-01 15:38:52 -0500

    Roll back to use single predicate method

--- Functional.Core/AsyncOperation.cs
+++ Functional.Core/AsyncOperation.cs
@@ -25,9 +25,9 @@ public class AsyncOperation<T>
     
     // Define a match function based on the result of the previous operation that 
     // allows us to branch the execution based a predicate of the result
-    public AsyncOperation<TResult> Match<TResult, TInner>(Func<T, TInner> result, Func<TInner, bool> predicate, Func<TInner, TResult> success, Func<TInner, TResult> failure)
+    public AsyncOperation<TResult> Match<TResult>(Func<T, bool> predicate, Func<T, AsyncOperation<TResult>> success, Func<T, AsyncOperation<TResult>> failure)
     {
-        return new AsyncOperation<TResult>(_task.ContinueWith(t => predicate(result(t.Result)) ? success(result(t.Result)) : failure(result(t.Result))));
+        return new AsyncOperation<TResult>(_task.ContinueWith(t => predicate(t.Result) ? success(t.Result)._task : failure(t.Result)._task).Unwrap());
     }
 
     public AsyncOperation<TResult> Tap<TResult>(Func<T, AsyncOperation<TResult>> selector)
